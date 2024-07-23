f124b1378321e94a5418a4434600484e2d26e71d
arislaode <laodearissaputra94@gmail.com>
2024-07-23 16:35:27 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 // Public routes
 Route::get('/', [HomeController::class, 'index']);
@@ -21,6 +22,17 @@ Route::prefix('admin')->middleware('auth')->name('admin.')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('dashboard');
     Route::get('/news', [AdminController::class, 'manageNews'])->name('news');
     Route::get('/categories', [AdminController::class, 'manageCategories'])->name('categories');
-    Route::get('/users', [AdminController::class, 'manageUsers'])->name('users');
+    
+    // User management routes
+    Route::resource('users', UserController::class);
+    // Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    // Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    // Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    // Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    // Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    // Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+
+
     Route::get('/settings', [AdminController::class, 'settings'])->name('settings');
 });
