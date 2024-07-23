efac99e6a096dbb2ccf08f8be898f9ce07f100ec
arislaode <laodearissaputra94@gmail.com>
2024-07-24 04:12:46 +0700

    update rull options using tinymce

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,34 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+            external: [
+                'tinymce',
+                'tinymce/themes/silver',
+                'tinymce/icons/default',
+                'tinymce/plugins/advlist',
+                'tinymce/plugins/autolink',
+                'tinymce/plugins/lists',
+                'tinymce/plugins/link',
+                'tinymce/plugins/image',
+                'tinymce/plugins/charmap',
+                'tinymce/plugins/print',
+                'tinymce/plugins/preview',
+                'tinymce/plugins/hr',
+                'tinymce/plugins/anchor',
+                'tinymce/plugins/pagebreak',
+                'tinymce/plugins/paste',
+                'tinymce/plugins/searchreplace',
+                'tinymce/plugins/code',
+                'tinymce/plugins/fullscreen',
+                'tinymce/plugins/insertdatetime',
+                'tinymce/plugins/media',
+                'tinymce/plugins/table',
+                'tinymce/plugins/code'
+            ]
+        }
+    },
     resolve: {
         alias: {
             '@': '/resources',
