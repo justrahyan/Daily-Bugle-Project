c3e8dfb904e17fe40bab7b311393d36b2ef2f1c0
arislaode <laodearissaputra94@gmail.com>
2024-07-25 08:56:31 +0700

    update home news

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="w-full mx-auto px-4 sm:px-6 lg:px-8">
         <!-- Start of new carousel -->
-        <div id="default-carousel" class="relative w-full mb-8" data-carousel="slide">
+        <div id="default-carousel"  class="relative w-full mb-8" data-carousel="slide" data-carousel-interval="6000">
             <!-- Carousel wrapper -->
             <div class="relative h-64 overflow-hidden rounded-lg md:h-80 lg:h-96">
                 @foreach ($news->take(5) as $index => $newsItem)
                     <div class="{{ $index === 0 ? '' : 'hidden' }} duration-700 ease-in-out" data-carousel-item>
-                        <img src="{{ $newsItem['image_url'] }}" class="absolute block w-full h-full object-cover" alt="{{ $newsItem['title'] }}">
+                        <img src="{{ asset('storage/' . $newsItem->image_url) }}" class="absolute block w-full h-full object-cover" alt="{{ $newsItem['title'] }}">
                         <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 w-full p-4">
                             <h2 class="text-white text-lg md:text-xl lg:text-2xl font-bold">{{ $newsItem['title'] }}</h2>
                         </div>
@@ -41,13 +41,13 @@
         </div>
         <!-- End of new carousel -->
 
-        <h1 class="text-2xl font-bold mb-4">News List</h1>
+        <h1 class="text-2xl font-bold mb-4">Berita Terpopuler</h1>
         <div id="news-list" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($paginatedNews as $newsItem)
                 <a href="{{ route('news.detail', $newsItem['id']) }}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 transition-transform transform hover:scale-105">
-                    <img class="object-cover w-full h-40 rounded-t-lg" src="{{ $newsItem['image_url'] }}" alt="{{ $newsItem['title'] }}">
+                    <img class="object-cover w-full h-40 rounded-t-lg" src="{{ asset('storage/' . $newsItem->image_url) }}" alt="{{ $newsItem['title'] }}">
                     <div class="flex flex-col justify-between p-4 leading-normal text-center">
-                        <p class="text-sm text-gray-600 dark:text-gray-400 mb-1">{{ \Carbon\Carbon::parse($newsItem['date'])->format('F j, Y') }} - {{ $newsItem['category'] }}</p>
+                    <p class="text-sm text-gray-600 dark:text-gray-400 mb-1">{{ \Carbon\Carbon::parse($newsItem['date'])->format('F j, Y') }} - {{ $newsItem->category->name }}</p>
                         <h5 class="mb-1 text-lg font-semibold tracking-tight text-gray-900 dark:text-white">{{ $newsItem['title'] }}</h5>
                     </div>
                 </a>
@@ -74,62 +74,79 @@
     </div>
     <div id="default-tab-content">
         <div class="p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="editor-choice" role="tabpanel" aria-labelledby="editor-choice-tab">
-            <div class="mb-4">
-                <!-- Gambar utama dari editor's choice dihilangkan -->
-                {{-- <img src="{{ $editorChoiceMain['image_url'] }}" alt="{{ $editorChoiceMain['title'] }}" class="w-full h-auto rounded-lg mb-4"> --}}
-                <h2 class="text-xl font-bold">{{ $editorChoiceMain['title'] }}</h2>
-                <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($editorChoiceMain['date'])->format('F j, Y') }}</p>
-            </div>
+            @if ($editorChoiceMain)
+                <div class="mb-4">
+                    {{-- <img src="{{ asset('storage/' . $editorChoiceMain->image_url) }}" alt="{{ $editorChoiceMain->title }}" class="w-full h-auto rounded-lg mb-4"> --}}
+                    <h2 class="text-xl font-bold">{{ $editorChoiceMain->title }}</h2>
+                    <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($editorChoiceMain->date)->format('F j, Y') }}</p>
+                </div>
+            @else
+                <div class="mb-4">
+                    <h2 class="text-xl font-bold">Belum ada tulisan feature</h2>
+                </div>
+            @endif
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 @foreach ($editorChoiceNews as $newsItem)
-                    <a href="{{ route('news.detail', $newsItem['id']) }}" class="flex items-center space-x-4">
-                        <img src="{{ $newsItem['image_url'] }}" alt="{{ $newsItem['title'] }}" class="w-16 h-16 rounded-lg">
+                    <a href="{{ route('news.detail', $newsItem->id) }}" class="flex items-center space-x-4">
+                        <img src="{{ asset('storage/' . $newsItem->image_url) }}" alt="{{ $newsItem->title }}" class="w-16 h-16 rounded-lg">
                         <div>
-                            <h3 class="text-md font-semibold">{{ $newsItem['title'] }}</h3>
-                            <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($newsItem['date'])->format('F j, Y') }}</p>
+                            <h3 class="text-md font-semibold">{{ $newsItem->title }}</h3>
+                            <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($newsItem->date)->format('F j, Y') }}</p>
                         </div>
                     </a>
                 @endforeach
             </div>
         </div>
         <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="komunitas" role="tabpanel" aria-labelledby="komunitas-tab">
-            <div class="mb-4">
-                <!-- Gambar utama dari komunitas dihilangkan -->
-                {{-- <img src="{{ $komunitasMain['image_url'] }}" alt="{{ $komunitasMain['title'] }}" class="w-full h-auto rounded-lg mb-4"> --}}
-                <h2 class="text-xl font-bold">{{ $komunitasMain['title'] }}</h2>
-                <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($komunitasMain['date'])->format('F j, Y') }}</p>
-            </div>
+            @if ($komunitasMain)
+                <div class="mb-4">
+                    {{-- <img src="{{ asset('storage/' . $komunitasMain->image_url) }}" alt="{{ $komunitasMain->title }}" class="w-full h-auto rounded-lg mb-4"> --}}
+                    <h2 class="text-xl font-bold">{{ $komunitasMain->title }}</h2>
+                    <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($komunitasMain->date)->format('F j, Y') }}</p>
+                </div>
+            @else
+                <div class="mb-4">
+                    <h2 class="text-xl font-bold">Belum ada tulisan komunitas</h2>
+                </div>
+            @endif
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 @foreach ($komunitasNews as $newsItem)
-                    <a href="{{ route('news.detail', $newsItem['id']) }}" class="flex items-center space-x-4">
-                        <img src="{{ $newsItem['image_url'] }}" alt="{{ $newsItem['title'] }}" class="w-16 h-16 rounded-lg">
+                    <a href="{{ route('news.detail', $newsItem->id) }}" class="flex items-center space-x-4">
+                        <img src="{{ asset('storage/' . $newsItem->image_url) }}" alt="{{ $newsItem->title }}" class="w-16 h-16 rounded-lg">
                         <div>
-                            <h3 class="text-md font-semibold">{{ $newsItem['title'] }}</h3>
-                            <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($newsItem['date'])->format('F j, Y') }}</p>
+                            <h3 class="text-md font-semibold">{{ $newsItem->title }}</h3>
+                            <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($newsItem->date)->format('F j, Y') }}</p>
                         </div>
                     </a>
                 @endforeach
             </div>
         </div>
         <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="opini" role="tabpanel" aria-labelledby="opini-tab">
-            <div class="mb-4">
-                <!-- Gambar utama dari opini dihilangkan -->
-                {{-- <img src="{{ $opiniMain['image_url'] }}" alt="{{ $opiniMain['title'] }}" class="w-full h-auto rounded-lg mb-4"> --}}
-                <h2 class="text-xl font-bold">{{ $opiniMain['title'] }}</h2>
-                <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($opiniMain['date'])->format('F j, Y') }}</p>
-            </div>
+            @if ($opiniMain)
+                <div class="mb-4">
+                    <!-- Gambar utama dari opini dihilangkan -->
+                    {{-- <img src="{{ asset('storage/' . $opiniMain->image_url) }}" alt="{{ $opiniMain->title }}" class="w-full h-auto rounded-lg mb-4"> --}}
+                    <h2 class="text-xl font-bold">{{ $opiniMain->title }}</h2>
+                    <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($opiniMain->date)->format('F j, Y') }}</p>
+                </div>
+            @else
+                <div class="mb-4">
+                    <h2 class="text-xl font-bold">Belum ada tulisan opini</h2>
+                </div>
+            @endif
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 @foreach ($opiniNews as $newsItem)
-                    <a href="{{ route('news.detail', $newsItem['id']) }}" class="flex items-center space-x-4">
-                        <img src="{{ $newsItem['image_url'] }}" alt="{{ $newsItem['title'] }}" class="w-16 h-16 rounded-lg">
+                    <a href="{{ route('news.detail', $newsItem->id) }}" class="flex items-center space-x-4">
+                        <img src="{{ asset('storage/' . $newsItem->image_url) }}" alt="{{ $newsItem->title }}" class="w-16 h-16 rounded-lg">
                         <div>
-                            <h3 class="text-md font-semibold">{{ $newsItem['title'] }}</h3>
-                            <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($newsItem['date'])->format('F j, Y') }}</p>
+                            <h3 class="text-md font-semibold">{{ $newsItem->title }}</h3>
+                            <p class="text-sm text-gray-500 dark:text-gray-400">{{ \Carbon\Carbon::parse($newsItem->date)->format('F j, Y') }}</p>
                         </div>
                     </a>
                 @endforeach
             </div>
         </div>
     </div>
+
     <!-- End of tabs -->
 @endsection
